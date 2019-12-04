1cf43937cdd12d71eafdc27756664b60476ee4e6
ODK <odk1193@gmail.com>
2019-12-04 20:41:26 +0900

    거의마지막

--- AStarStationProject/Assets/02.Scripts/AStar.cs
+++ AStarStationProject/Assets/02.Scripts/AStar.cs
@@ -21,6 +21,9 @@ public class AStar : MonoBehaviour
     private CameraMove camMove;
     private TouchMgr touchMgr;
     private StationData stationData;
+    private bool isEnd;
+    private bool isSearching;
+    private IEnumerator coru;
 
     void Start()
     {
@@ -51,46 +54,38 @@ public class AStar : MonoBehaviour
         H = Vector2.Distance(st.GetPos(), destination.GetPos());
         F = G + H;
         st.SetG(G);
-        st.SetG(H);
-        st.SetG(F);
+        st.SetH(H);
+        st.SetF(F);
         st.SetParentName(nowStation.GetStationName());
     }
 
     public IEnumerator SearchPath(string start, string end)
     {
+        isEnd = false;
         panel.SetActive(true);
-        Debug.Log("0");
         nowStation = stationData.GetStation(start);
         destination = stationData.GetStation(end);
         openedList.Add(nowStation);
 
-        // TODO: 이하, 테스트용.
-        // yield return new WaitForSeconds(2.0f);
-        // List<Station> finalList = new List<Station>();
-        // finalList.Add(nowStation);
-        // finalList.Add(destination);
-        // routeList = ChangeStToTr(finalList);
-        // panel.SetActive(false);
-        // yield return StartCoroutine("RouteAnim");
-        // touchMgr.canTouch = true;
-
-        // TODO: 이하, 메소드 작성 후 주석 풀기
-        Debug.Log("0.5");
-        LoopSearch();
-
-        // test
-        touchMgr.canTouch = true;
-        foreach (Station s in closedList)
+        while (!isEnd)
         {
-            Debug.Log(s.GetStationName());
+            if (isSearching)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                yield return StartCoroutine("LoopSearch");
+            }
         }
-        panel.SetActive(false);
-        // test
+
+        // TODO: 실제로는 필요한 코드. 아랫줄과 대체.
         // List<Station> finalList = GetFinalRouteList();
-        // routeList = ChangeStToTr(finalList);
-        // panel.SetActive(false);
-        // yield return StartCoroutine("RouteAnim");
-        // touchMgr.canTouch = true;
+        List<Station> finalList = closedList;
+        routeList = ChangeStToTr(finalList);
+        panel.SetActive(false);
+        yield return StartCoroutine("RouteAnim");
+        touchMgr.canTouch = true;
         yield return null;
     }
 
@@ -104,41 +99,41 @@ public class AStar : MonoBehaviour
         return trList;
     }
 
-    private void LoopSearch()
+    private IEnumerator LoopSearch()
     {
-        float endTime = Time.time + 5;
-        Debug.Log("1");
-        while (endTime > Time.time)
+        isSearching = true;
+        // 현재역을 클로즈리스트에 넣음
+        // 클로즈리스트에 전에 오픈리스트에서 제거
+        closedList.Add(nowStation);
+        openedList.Remove(nowStation);
+        foreach (ConnStation cs in nowStation.GetConnStationList())
         {
-            Debug.Log("2");
-            foreach (ConnStation cs in nowStation.GetConnStationList())
+            Station st = stationData.GetStation(cs.GetStationName());
+            SetStationLists(st);
+            if (st.GetStationName().Equals(destination.GetStationName()))
             {
-                Debug.Log("3");
-                Station st = stationData.GetStation(cs.GetStationName());
-                Debug.Log("st: " + st);
-                SetStationLists(st);
-                if (st.GetStationName().Equals(destination.GetStationName()))
-                {
-                    Debug.Log("4");
-                    destination.SetParentName(nowStation.GetStationName());
-                    return;
-                }
+                destination.SetParentName(nowStation.GetStationName());
+                isEnd = true;
+                StopCoroutine("LoopSearch");
+                break;
             }
-            if (openedList.Count == 0)
-            {
-                return;
-            }
+        }
+        if (openedList.Count == 0)
+        {
+            destination.SetParentName(nowStation.GetStationName());
+            isEnd = true;
+            StopCoroutine("LoopSearch");
+        }
+        if (!isEnd)
+        {
             nowStation = GetNearestStation();
+            isSearching = false;
+            yield return null;
         }
     }
 
     private void SetStationLists(Station st) // 탐색시작시 호출하는 메소드
     {
-        // 현재역을 클로즈리스트에 넣음
-        // 클로즈리스트에 전에 오픈리스트에서 제거
-        openedList.Remove(st);
-        closedList.Add(st);
-
         // 현재역의 인접역이 닫힌목록에 있으면 True 없으면 False
         bool nowStationInClosedList = (closedList.Find(item => item.GetStationName().Equals(st.GetStationName())) != null);
         // 현재역의 인접역이 열린목록에 있으면 True 없으면 False
@@ -150,13 +145,12 @@ public class AStar : MonoBehaviour
         else if (nowStationInOpenedList) CheckRouteImproveRequired(st);
         // 열린목록에 없으면 열린목록에 추가하는 메소드 호출
         else AddNowStationToOpenList(st);
-
     }
 
     private void CheckRouteImproveRequired(Station st) // 경로개선 메소드
     {
         FGHCalculation(st);
-        Debug.Log("find: " + openedList.Find(item => item.GetStationName().Equals(st.GetStationName())));
+        // Debug.Log("find: " + openedList.Find(item => item.GetStationName().Equals(st.GetStationName())));
         float originalG = openedList.Find(item => item.GetStationName().Equals(st.GetStationName())).GetG();
         float nowG = st.GetG();
         // 기존 오픈리스트에 있던 station G값보다 현재 station G값이 작으면 기존 station 삭제, 현재 station 추가
@@ -176,13 +170,14 @@ public class AStar : MonoBehaviour
     private Station GetNearestStation()
     {
         float highestF = (from openData in openedList select openData.GetF()).Min();
+        Station s = openedList.Find(item => item.GetF().Equals(highestF));
         return openedList.Find(item => item.GetF().Equals(highestF));
     }
 
     private List<Station> GetFinalRouteList()
     {
         List<Station> finalList = new List<Station>();
-        Station nextStation = closedList[closedList.Count -1];
+        Station nextStation = destination;
         while (!closedList[0].GetStationName().Equals(nextStation.GetStationName()))
         {
             if (nextStation.GetStationName().Equals(destination.GetStationName()))
